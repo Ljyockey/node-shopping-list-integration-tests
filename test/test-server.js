a5688a06cf456bbb8ba14d037b4f5160c6d2f802
ljyockey <linyockey@gmail.com>
2017-04-26 14:02:51 -0700

    added GET and POST tests

--- test/test-server.js
+++ test/test-server.js
@@ -142,3 +142,44 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('Recipes', function() {
+  before(function() {
+    return runServer();
+  });
+  after(function() {
+    return closeServer();
+  });
+  it('should list all Recipe items in GET request', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.length.should.be.at.least(1);
+      const expectedKeys = ['id', 'name', 'ingredients'];
+      res.body.forEach(function(item) {
+        item.should.be.a('object');
+        item.should.include.keys(expectedKeys);
+      });
+    });
+  });
+  it('should add Recipe items in POST request', function() {
+    const newItem = {name: 'lemonade', ingredients: ['lemons', 'water', 'sugar']};
+    return chai.request(app)
+    .post('/recipes')
+    .send(newItem)
+    .then(function(res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('id', 'name', 'ingredients');
+      res.body.id.should.not.be.null;
+      res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+    });
+  });
+});
+
+
+
